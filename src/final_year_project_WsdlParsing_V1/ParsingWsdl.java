7110d1542a24937b73d66f0b7ca1dc51d21bccdc
my301 <my301@itl115.student.eecs.qmul.ac.uk>
2014-11-18 11:44:32 +0000

    1st version 18/11/2014 11:43
    sorting out the complex datatype.

--- src/final_year_project_WsdlParsing_V1/ParsingWsdl.java
+++ src/final_year_project_WsdlParsing_V1/ParsingWsdl.java
@@ -4,8 +4,15 @@
  */
 package final_year_project_WsdlParsing_V1;
 
+import com.predic8.schema.Appinfo;
+import com.predic8.schema.Attribute;
+import com.predic8.schema.ComplexContent;
 import com.predic8.schema.ComplexType;
+import com.predic8.schema.Derivation;
 import com.predic8.schema.Element;
+import com.predic8.schema.ModelGroup;
+import com.predic8.schema.SchemaComponent;
+import com.predic8.schema.SimpleType;
 import com.predic8.soamodel.Consts;
 import com.predic8.wsdl.*;
 import java.util.ArrayList;
@@ -39,10 +46,7 @@ public class ParsingWsdl {
                 System.out.println(pt.getName());
                 
                 for (Operation op : pt.getOperations()) {
-                    
-                    
-                    
-                    
+
                     System.out.println("-444444444444-" + op.getName());
                     
                     System.out.println("22221222");
@@ -101,92 +105,40 @@ public class ParsingWsdl {
         
     }
     
+    
     private static void listParameters(Element element) {
         
         
-        
         ComplexType ct = (ComplexType) element.getEmbeddedType();
-        if (ct == null){
-            
-            ///ct = element.getSchema().getComplexType(element.getType().getLocalPart());
-            
-            out("***************************************************************");
-            
-            out("  Schema ComplexTypes: ");
-            for (ComplexType ct1 : element.getSchema().getComplexTypes()) {
-                out("    ComplexType Name: " + ct1.getName());
-                if (ct1.getAnnotation() != null)
-                    annotationOut(ct1);
-                if (ct1.getAttributes().size() > 0) {
-                    out("    ComplexType Attributes: ");
-                    /*
-                     * If available, attributeGroup could be read as same as attribute in
-                     * the following.
-                     */
-                    for (Attribute attr : ct1.getAttributes()) {
-                        out("      Attribute Name: " + attr.getName());
-                        out("      Attribute Form: " + attr.getForm());
-                        out("      Attribute ID: " + attr.getId());
-                        out("      Attribute Use: " + attr.getUse());
-                        out("      Attribute FixedValue: " + attr.getFixedValue());
-                        out("      Attribute DefaultValue: " + attr.getDefaultValue());
-                    }
-                }
-                /*
-                 * ct1.getModel() delivers the child element used in complexType. In case
-                 * of 'sequence' you can also use the getSequence() method.
-                 */
-                out("    ComplexType Model: " + ct1.getModel().getClass().getSimpleName());
-                if (ct1.getModel() instanceof ModelGroup) {
-                    out("    Model Particles: ");
-                    for (SchemaComponent sc : ((ModelGroup) ct1.getModel()).getParticles()) {
-                        out("      Particle Kind: " + sc.getClass().getSimpleName());
-                        out("      Particle Name: " + sc.getName() + "\n");
-                    }
+        if (ct == null){            
+            try{                
+                ct = element.getSchema().getComplexType(element.getType().getLocalPart());
+               
+            }catch(Exception e){
+                
+                if (element.getSchema().getSimpleTypes().size() > 0) {
+                    out("  Schema SimpleTypes: ");
+                    for (SimpleType st : element.getSchema().getSimpleTypes()) {
+                        out("    SimpleType Name: " + st.getName());
+                        out("    SimpleType Restriction: " + st.getRestriction());
+                        out("    SimpleType Union: " + st.getUnion());
+                        out("    SimpleType List: " + st.getList());
+                    } 
                 }
                 
-                if (ct1.getModel() instanceof ComplexContent) {
-                    Derivation der = ((ComplexContent) ct1.getModel()).getDerivation();
-                    out("      ComplexConten Derivation: " + der.getClass().getSimpleName());
-                    out("      Derivation Base: " + der.getBase());
-                }
-                
-                if (ct1.getAbstractAttr() != null)
-                    out("    ComplexType AbstractAttribute: " + ct1.getAbstractAttr());
-                if (ct1.getAnyAttribute() != null)
-                    out("    ComplexType AnyAttribute: " + ct1.getAnyAttribute());
-                
-                out("");
+                System.out.println("****************************the end*************************************");
+                return;
             }
-            
-            if (schema.getSimpleTypes().size() > 0) {
-                out("  Schema SimpleTypes: ");
-                for (SimpleType st : schema.getSimpleTypes()) {
-                    out("    SimpleType Name: " + st.getName());
-                    out("    SimpleType Restriction: " + st.getRestriction());
-                    out("    SimpleType Union: " + st.getUnion());
-                    out("    SimpleType List: " + st.getList());
-                }
-            }
-            
-            
-            out("***********************************************************************************");
-            
-            
-            
+           
         }
         if(ct.getModel()!=null){
             
             for (Element e : ct.getSequence().getElements()) {
                 
-                // Fix for invalid schema
-                
+                // Fix for invalid schema                
                 if (e.getType() == null) {
-                    
                     System.out.println("Element eeeeeeeeeeeeeeeeeeee: " + element.getName());
-                    
                     return;
-                    
                 }
                 
                 if (e.getType().getNamespaceURI() == Consts.SCHEMA_NS) {
@@ -195,8 +147,8 @@ public class ParsingWsdl {
                     
                 } else {
                     
+                    out("eeeeeeeeeeeeeeeeeeeeee: "+e.getType());                   
                     listParameters(e);
-                    
                 }
                 
             }
@@ -204,8 +156,90 @@ public class ParsingWsdl {
         
     }
     
+    // delete this method
+    private static void completextype(Element element)
+    {
+        out("*****************************remove the stuff between the starts and comment the top line **********************************");
+        
+        out("  Schema ComplexTypes: ");
+        for (ComplexType ct1 : element.getSchema().getComplexTypes()) {
+            out("    ComplexType Name: " + ct1.getName());
+            if (ct1.getAnnotation() != null)
+                annotationOut(ct1);
+            if (ct1.getAttributes().size() > 0) {
+                out("    ComplexType Attributes: ");
+                /*
+                 * If available, attributeGroup could be read as same as attribute in
+                 * the following.
+                 */
+                for (Attribute attr : ct1.getAttributes()) {
+                    out("      Attribute Name: " + attr.getName());
+                    out("      Attribute Form: " + attr.getForm());
+                    out("      Attribute ID: " + attr.getId());
+                    out("      Attribute Use: " + attr.getUse());
+                    out("      Attribute FixedValue: " + attr.getFixedValue());
+                    out("      Attribute DefaultValue: " + attr.getDefaultValue());
+                }
+            }
+            /*
+             * ct1.getModel() delivers the child element used in complexType. In case
+             * of 'sequence' you can also use the getSequence() method.
+             */
+            out("    ComplexType Model: " + ct1.getModel().getClass().getSimpleName());
+            if (ct1.getModel() instanceof ModelGroup) {
+                out("    Model Particles: ");
+                for (SchemaComponent sc : ((ModelGroup) ct1.getModel()).getParticles()) {
+                    out("      Particle Kind: " + sc.getClass().getSimpleName());
+                    //out("      particle Type: " + sc.);
+                    out("      Particle Name: " + sc.getName() + "\n");
+                }
+            }
+            
+            if (ct1.getModel() instanceof ComplexContent) {
+                Derivation der = ((ComplexContent) ct1.getModel()).getDerivation();
+                out("      ComplexConten Derivation: " + der.getClass().getSimpleName());
+                out("      Derivation Base: " + der.getBase());
+            }
+            
+            if (ct1.getAbstractAttr() != null)
+                out("    ComplexType AbstractAttribute: " + ct1.getAbstractAttr());
+            if (ct1.getAnyAttribute() != null)
+                out("    ComplexType AnyAttribute: " + ct1.getAnyAttribute());
+            
+            out("");
+        }
+        
+        if (element.getSchema().getSimpleTypes().size() > 0) {
+            out("  Schema SimpleTypes: ");
+            for (SimpleType st : element.getSchema().getSimpleTypes()) {
+                out("    SimpleType Name: " + st.getName());
+                out("    SimpleType Restriction: " + st.getRestriction());
+                out("    SimpleType Union: " + st.getUnion());
+                out("    SimpleType List: " + st.getList());
+            }
+        }
+        
+        
+        out("***********************************************************************************");
+        
+    }
     
-    
+     // delete this method
+    private static void annotationOut(SchemaComponent sc) {
+        if (sc.getAnnotation().getAppinfos().size() > 0) {
+            System.out
+                    .print("    Annotation (appinfos) available with the content: ");
+            for (Appinfo appinfo : sc.getAnnotation().getAppinfos()) {
+                out(appinfo.getContent());
+            }
+        } else {
+            System.out
+                    .print("    Annotation (documentation) available with the content: ");
+            for (com.predic8.schema.Documentation doc : sc.getAnnotation().getDocumentations()) {
+                out(doc.getContent());
+            }
+        }
+    }
     
     private static void out(String str) {
         System.out.println(str);
