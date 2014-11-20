793f34ac945db1bac73228e46bdeaba8b3b6343f
my301 <my301@itl115.student.eecs.qmul.ac.uk>
2014-11-20 16:51:35 +0000

    1st version 20/11/2014 16:51
    Solved the restriction  problem.
    
    currently working on how to store this.

--- src/final_year_project_GUI_v1/WelcomePage.java
+++ src/final_year_project_GUI_v1/WelcomePage.java
@@ -141,6 +141,9 @@ public class WelcomePage extends javax.swing.JFrame {
         //      http://www.webservicex.net/sendsmsworld.asmx?WSDL
         //      http://www.thomas-bayer.com/axis2/services/BLZService?wsdl
         //      http://trial.serviceobjects.com/gp/GeoPhone.asmx?wsdl
+        //       http://www.webservicex.net/WeatherForecast.asmx?WSDL
+        //      /homes/my301/year3/Project/other/GoogleSearch.wsdl
+
         
         wsdlInput.setText("http://www.webservicex.net/genericbarcode.asmx?WSDL");
  
