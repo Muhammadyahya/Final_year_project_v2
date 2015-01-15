5b5eb08896661427e49b6ff858308bff46885031
my301 <my301@itl115.student.eecs.qmul.ac.uk>
2015-01-15 16:30:00 +0000

    added Char GUI
    15/01/2015 16:29

---
+++ src/Logic/CheckResult/ResultChecker.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic.CheckResult;
+
+/**
+ *
+ * @author my301
+ */
+public class ResultChecker {
+    
+    public void AfterReceiveReply(String reply, Object correlationState)
+    {
+        
+        /*
+        // Write out http headers
+        foreach (var property in reply.Properties)
+        {
+            if (!(property.Value is HttpResponseMessageProperty)) continue;
+            var httpProperties = (HttpResponseMessageProperty)property.Value;
+            foreach (KeyValuePair<object, object> kvp in httpProperties.Headers)
+            {
+                Console.WriteLine(kvp.Key + ":" + kvp.Value);
+            }
+        }
+        // Write result message
+        Console.WriteLine(reply.ToString());
+        
+    */
+    }    
+}
--- src/aDeleteME/SoapUIRequest.java
+++ src/aDeleteME/SoapUIRequest.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package aExamples;
+package aDeleteME;
 
 import com.eviware.soapui.SoapUI;
 import com.eviware.soapui.StandaloneSoapUICore;
