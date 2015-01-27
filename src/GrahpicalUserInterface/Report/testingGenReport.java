42b138fb203a4f9ee345d86503672c5142d23c99
my301 <my301@itl115.student.eecs.qmul.ac.uk>
2015-01-27 12:41:03 +0000

    Latest Version
    27/01/2015 12:40

--- src/GrahpicalUserInterface/Report/testingGenReport.java
+++ src/GrahpicalUserInterface/Report/testingGenReport.java
@@ -5,9 +5,7 @@
 package GrahpicalUserInterface.Report;
 
 import java.io.FileNotFoundException;
-import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
-import net.sf.dynamicreports.report.builder.DynamicReports;
-import net.sf.dynamicreports.report.exception.DRException;
+
 
 
 /**
@@ -16,15 +14,9 @@ import net.sf.dynamicreports.report.exception.DRException;
  */
 public class testingGenReport {
     
-    public static void main(String [] agrs) throws DRException, FileNotFoundException{
+    public static void main(String [] agrs) throws FileNotFoundException{
         
-       // dynamic report
-		JasperReportBuilder report = DynamicReports.report();
-
-		
-
-        report.show();
-        
+   
         
     }
     
